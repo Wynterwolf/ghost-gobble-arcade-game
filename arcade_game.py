ec1013f8fae440ece026ad36baf816b704e3287a
Samantha Jordan <shinyguns@gmail.com>
2024-08-23 21:22:18 -0500

    passing tests

--- arcade_game.py
+++ arcade_game.py
@@ -11,6 +11,8 @@ def eat_ghost(power_pellet_active, touching_ghost):
 
     if power_pellet_active and touching_ghost:
         return True
+    else:
+        return False
 
 
 def score(touching_power_pellet, touching_dot):
@@ -21,7 +23,10 @@ def score(touching_power_pellet, touching_dot):
     :return: bool - has the player scored or not?
     """
 
-    pass
+    if touching_dot or touching_power_pellet:
+        return True
+    else:
+        return False
 
 
 def lose(power_pellet_active, touching_ghost):
@@ -31,8 +36,16 @@ def lose(power_pellet_active, touching_ghost):
     :param touching_ghost: bool - is the player touching a ghost?
     :return: bool - has the player lost the game?
     """
-
-    pass
+    # The player does not lose if they are touching a ghost and have an active power pellet
+    if touching_ghost and power_pellet_active:
+        return False
+    
+    # The player loses if they are touching a ghost and there is no power pellet active
+    if touching_ghost and not power_pellet_active:
+        return True
+    
+    # The player does not lose in any other case
+    return False
 
 
 def win(has_eaten_all_dots, power_pellet_active, touching_ghost):
@@ -44,4 +57,14 @@ def win(has_eaten_all_dots, power_pellet_active, touching_ghost):
     :return: bool - has the player won the game?
     """
 
-    pass
+    # The player cannot win if they are touching a ghost and don't have a power pellet active
+    if touching_ghost and not power_pellet_active:
+        return False
+
+    # The player wins if they have eaten all the dots and are either not touching a ghost
+    # or have a power pellet active
+    if has_eaten_all_dots:
+        return True
+
+    # Otherwise, the player does not win
+    return False
